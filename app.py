077d9c6f5bd42360161e5fe80097754fe690df82
kemeny <francisco@kemeny.cl>
2023-11-06 17:37:35 -0600

    req

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import openai
 import os
 import time
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # Initialize the environment variables and OpenAI API client
-#load_dotenv()
+load_dotenv()
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = st.secrets['openai']["OPENAI_API_KEY"]
 
